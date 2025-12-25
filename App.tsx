a9e03839d89b8257b380f187c340e22ddc05d6b1
1126misakp <124807530+1126misakp@users.noreply.github.com>
2025-12-25 13:29:06 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import KnowledgeGraph3D from './components/KnowledgeGraph3D';
+
+export default function App() {
+  return (
+    <div className="w-full h-screen bg-slate-950">
+      <KnowledgeGraph3D />
+    </div>
+  );
+}
